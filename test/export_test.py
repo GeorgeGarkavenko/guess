bcece7457ca4bbcfad93b58b34e7bcb1f792a6a8
Jaska <jaska@Jaakkos-iMac.local>
2016-05-31 18:39:52 +0300

    Added test for adjusment oid

--- test/export_test.py
+++ test/export_test.py
@@ -2,9 +2,14 @@ from unittest import TestCase
 
 
 class Adjustment(object):
-    pass
+    def get_oid(self):
+        pass
 
 
 class TestAdjustment(TestCase):
     def test_ajustment_can_be_created(self):
         a = Adjustment()
+
+    def test_adjustment_oid(self):
+        a = Adjustment()
+        oid = a.get_oid()
