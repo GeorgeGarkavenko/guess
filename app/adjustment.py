196b8353682a861e8e062fe9fc3a627a6dea21ae
Jaska <jaska@Jaakkos-iMac.local>
2016-06-01 13:16:42 +0300

    Added adjustment description

--- app/adjustment.py
+++ app/adjustment.py
@@ -1,6 +1,15 @@
 class Adjustment(object):
-    def get_oid(self):
-        pass
 
-    def get_description(self):
-        return ""
+    def __init__(self, oid, external_id, name, event, rule_name):
+        self.oid = oid
+        self.external_id = external_id
+        self.name = name
+        self.event = event
+        self.rule_name = rule_name
+
+class AdjustmentDescription(object):
+
+    def __init__(self, language_id, description, image):
+        self.language_id = language_id
+        self.description = description
+        self.image = image
--- test/export_test.py
+++ test/export_test.py
@@ -1,20 +1,46 @@
 from unittest import TestCase
 
-from app.adjustment import Adjustment
+from app.adjustment import Adjustment, AdjustmentDescription
 
 
 class TestAdjustment(TestCase):
 
     def setUp(self):
-        self.a = Adjustment()
+        self.a = Adjustment("A25E3EE9AFA248A79DF07D2565410784", "", "Back to school 10% off", "", "Promotion % Off")
 
-    def test_ajustment_can_be_created(self):
-        pass
+    # def test_ajustment_can_be_created(self):
+    #     pass
 
     def test_adjustment_oid(self):
-        oid = self.a.get_oid()
+        self.assertEqual(self.a.oid, "A25E3EE9AFA248A79DF07D2565410784")
+
+    def test_adjustment_name(self):
+        self.assertEqual(self.a.name, "Back to school 10% off")
+
+    def test_adjustment_external_id(self):
+        self.assertEqual(self.a.external_id, "")
+
+    def test_adjustment_event(self):
+        self.assertEqual(self.a.event, "")
+
+    def test_adjustment_rule_name(self):
+        self.assertEqual(self.a.rule_name, "Promotion % Off")
 
 
-    def test_adjustment_description(self):
-        d = self.a.get_description()
-        self.assertEqual(d, "", "Adjustment description is emtpy string")
+class TestAdjustmentDescription(TestCase):
+
+    def setUp(self):
+        self.d = AdjustmentDescription("Pen", "Back to school", "")
+
+    # def test_adjustment_description_can_be_created(self):
+    #     pass
+
+    def test_adjustment_description_language_id(self):
+        self.assertEqual(self.d.language_id, "Pen")
+
+    def test_adjustment_description_text(self):
+        self.assertEqual(self.d.description, "Back to school")
+
+
+    def test_adjustment_description_image(self):
+        self.assertEqual(self.d.image, "")
